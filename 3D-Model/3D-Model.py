f9b60fdd0af27ff6bfb98a490bd37f22137fab03
Simone La Bella <labella.1995847@studenti.uniroma1.it>
2024-07-11 13:06:40 +0200

    Gui almost completed

--- 3D-Model/3D-Model.py
+++ 3D-Model/3D-Model.py
@@ -35,7 +35,8 @@ brain_masked = np.copy(brain_volume)
 brain_masked[mask_volume != 0] = 0
 
 # Aggiunta del cervello come sfondo trasparente
-src_brain = mlab.pipeline.scalar_field(brain_masked[:150,:,:].astype(np.float64))
+#src_brain = mlab.pipeline.scalar_field(brain_masked[:150,:,:].astype(np.float64))
+src_brain = mlab.pipeline.scalar_field(brain_masked.astype(np.float64))
 brain_surface = mlab.pipeline.iso_surface(src_brain, color=(0.6, 0.79, 0.8), opacity=0.5)
 
 surfaces = []
@@ -45,6 +46,7 @@ for class_value, color in colors.items():
     if class_value == 0:
         continue  # Salta la classe 0 (nero)
     # Seleziona solo i voxel corrispondenti alla classe corrente
+    #mask = mask_volume[:150,:,:] == class_value
     mask = mask_volume[:150,:,:] == class_value
     
     # Creazione di una superficie isosurfacing per la classe corrente
@@ -56,5 +58,5 @@ for class_value, color in colors.items():
 
 mlab.view(azimuth=90, elevation=90, roll=0)
 
-mlab.savefig('results/prova1.obj', figure=mlab.gcf())
+mlab.savefig('results/brain.obj', figure=mlab.gcf())
 
